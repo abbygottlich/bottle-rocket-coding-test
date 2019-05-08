23bcacb5b74196de713e20fd829070fe3175e27b
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-08 16:32:01 -0500

    displaying restaurant names in browser

--- lunch-tyme/src/components/HomeComponent.js
+++ lunch-tyme/src/components/HomeComponent.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react';
 
 class Home extends Component {
 
-    componentDidMount() {
+    state = {
+        restaurantInfo: []
+    }
+
+    componentDidMount = () => {
+
+        let restaurantNames = []
+
         fetch('http://sandbox.bottlerocketapps.com/BR_iOS_CodingExam_2015_Server/restaurants.json')
-            .then(function (response) {
+            .then((response) => {
                 return response.json();
             })
-            .then(function (restaurantInfo) {
-                console.log(JSON.stringify(restaurantInfo));
-            });
+            .then((data) => {
+                console.log(data);
+                data.restaurants.map(info => restaurantNames.push(info.name))
+            })
+            .then(() => this.setState({
+                restaurantInfo: restaurantNames
+            }))
     }
 
     render() {
         return (
             <div className="App">
-                <div className="main"></div>
+                <div className="main">{this.state.restaurantInfo}</div>
             </div>
         );
     }
