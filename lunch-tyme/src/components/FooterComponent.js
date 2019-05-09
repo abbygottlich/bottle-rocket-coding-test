099b619882a6b9ec7258eab1c993267d780cc34c
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-08 21:27:39 -0500

    footer text and icons styled

--- lunch-tyme/src/components/FooterComponent.js
+++ lunch-tyme/src/components/FooterComponent.js
@@ -4,10 +4,23 @@ class Footer extends Component {
 
     render() {
         return (
+            // <div className="footer">
+            //     <div className="icons-and-text">
+            //         <div className="lunch">
+            //             <div className="lunch-icon"></div>
+            //             <div className="lunch-text">lunch</div>
+            //         </div>
+            //         <div className="internets">
+            //             <div className="internets-icon-2"></div>
+            //             <div className="internets-text">internets</div>
+            //         </div>
+            //     </div>
+            // </div>
+
             <div className="footer">
                 <div className="icons-and-text">
                     <div className="lunch">
-                        <div className="lunch-icon"></div>
+                        <div className="lunch-icon-2"></div>
                         <div className="lunch-text">lunch</div>
                     </div>
                     <div className="internets">
