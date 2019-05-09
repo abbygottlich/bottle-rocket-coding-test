5b808922e532700eed0fe2d806c488a085e3eb0c
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-09 14:11:57 -0500

    footer routes set up

--- lunch-tyme/src/components/FooterComponent.js
+++ lunch-tyme/src/components/FooterComponent.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 
 class Footer extends Component {
 
-    render() {
-        return (
-            // <div className="footer">
-            //     <div className="icons-and-text">
-            //         <div className="lunch">
-            //             <div className="lunch-icon"></div>
-            //             <div className="lunch-text">lunch</div>
-            //         </div>
-            //         <div className="internets">
-            //             <div className="internets-icon-2"></div>
-            //             <div className="internets-text">internets</div>
-            //         </div>
-            //     </div>
-            // </div>
-
-            <div className="footer">
-                <div className="icons-and-text">
-                    <div className="lunch">
-                        <div className="lunch-icon-2"></div>
-                        <div className="lunch-text">lunch</div>
-                    </div>
-                    <div className="internets">
-                        <div className="internets-icon"></div>
-                        <div className="internets-text">internets</div>
-                    </div>
+    // lunch footer
+    lunchFooter = () => {
+        return <div className="footer">
+            <div className="icons-and-text">
+                <div className="lunch">
+                    <div className="lunch-icon"></div>
+                    <div className="lunch-text">lunch</div>
+                </div>
+                <div className="internets">
+                    <div className="internets-icon-2"></div>
+                    <div className="internets-text">internets</div>
                 </div>
             </div>
+        </div>
+    }
+
+    // internets footer
+    internetsFooter = () => {
+        return <div className="footer">
+            <div className="icons-and-text">
+                <div className="lunch">
+                    <div className="lunch-icon-2"></div>
+                    <div className="lunch-text">lunch</div>
+                </div>
+                <div className="internets">
+                    <div className="internets-icon"></div>
+                    <div className="internets-text">internets</div>
+                </div>
+            </div>
+        </div>
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <Route exact path='/' component={this.lunchFooter} />
+                <Route path='/detail' component={this.lunchFooter} />
+                <Route path='/contact' component={this.internetsFooter} />
+            </BrowserRouter>
         );
     }
 }
