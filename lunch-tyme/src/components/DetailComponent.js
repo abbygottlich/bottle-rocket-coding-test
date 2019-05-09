e1c4f04e21da65ccf0d61003660fc042f2072783
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-09 13:28:13 -0500

    all restaurant info rendering conditionally

--- lunch-tyme/src/components/DetailComponent.js
+++ lunch-tyme/src/components/DetailComponent.js
@@ -12,11 +12,16 @@ class Detail extends Component {
                 </div>
                 <div className="restaurant-info">
                     <div className="address">
-                        <div className="address-street">{this.props.singleRestaurant.location.address}</div>
-                        <div className="address-city-state">{this.props.singleRestaurant.location.city}, {this.props.singleRestaurant.location.state} {this.props.singleRestaurant.location.postalCode}</div>
+                        <div className="address-street">{this.props.singleRestaurant.location.address ? this.props.singleRestaurant.location.address : null}</div>
+                        <div className="address-city-state">
+                            {/* if there is a city AND state, return the city with a comma, else, return the city without a comma */}
+                            <span>{this.props.singleRestaurant.location.city && this.props.singleRestaurant.location.state ? this.props.singleRestaurant.location.city + ", " : this.props.singleRestaurant.location.city && !this.props.singleRestaurant.location.state ? this.props.singleRestaurant.location.city : null}</span>
+                            <span>{this.props.singleRestaurant.location.state ? this.props.singleRestaurant.location.state : null}</span>
+                            <span>{this.props.singleRestaurant.location.postalCode ? " " + this.props.singleRestaurant.location.postalCode : null}</span>
+                        </div>
                     </div>
-                    <div className="number">{this.props.singleRestaurant.contact.formattedPhone}</div>
-                    <div className="twitter">@{this.props.singleRestaurant.contact.twitter}</div>
+                    <div className="number">{this.props.singleRestaurant.contact && this.props.singleRestaurant.contact.formattedPhone ? this.props.singleRestaurant.contact.formattedPhone : null}</div>
+                    <div className="twitter">{this.props.singleRestaurant.contact && this.props.singleRestaurant.contact.twitter ? "@" + this.props.singleRestaurant.contact.twitter : null}</div>
                 </div>
             </div>
         );
