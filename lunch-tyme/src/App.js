a2422a59ed940d71a357ca031daee6e558b88ced
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-08 21:37:18 -0500

    added padding to main and imported react-router-dom

--- lunch-tyme/src/App.js
+++ lunch-tyme/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./components/HeaderComponent";
 import HomeComponent from "./components/HomeComponent";
 import Footer from "./components/FooterComponent"
 
 function App() {
   return (
-    <div className="App">
-      <div className="header-container">
-        <Header />
+    <BrowserRouter>
+      <div className="App">
+        <div className="header-container">
+          <Header />
+        </div>
+        <Route exact path="/" component={HomeComponent} />
+        <div className="footer-container">
+          <Footer />
+        </div>
       </div>
-      <HomeComponent />
-      <div className="footer-container">
-        <Footer />
-      </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
